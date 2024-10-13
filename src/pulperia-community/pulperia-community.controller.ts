ab843b0d107b46965d95e2fd81e6fc3a8f0228f0
fabjiro <fjiron2001@gmail.com>
2024-10-13 16:53:10 -0600

    user community delete controller

--- src/pulperia-community/pulperia-community.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { PulperiaCommunityService } from './pulperia-community.service';
-
-@Controller('pulperia-community')
-export class PulperiaCommunityController {
-  constructor(private readonly pulperiaCommunityService: PulperiaCommunityService) {}
-}
